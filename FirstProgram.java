d10d3b3e9a40756bda8c2c61c1ec31537c4f40d2
mallela009 <92942717+mallela009@users.noreply.github.com>
2022-10-21 19:14:46 +0530

    Add files via upload

---
+++ FirstProgram.java
@@ -0,0 +1,13 @@
+package javabasic;
+
+public class FirstProgram {
+	public static void main(String []args) {
+	 String name="mallela nagarjuna reddy";
+	 String father_Name="mallela ramasubba reddy";
+	 String address="Dhodium village";
+	 System.out.println(name);
+	 System.out.println(father_Name);
+	 System.out.println(address);
+	}
+
+}
